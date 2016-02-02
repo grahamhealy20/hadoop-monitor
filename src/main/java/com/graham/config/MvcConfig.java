207de0f733606478d9ddf60d03aa0feeedd19aa1
Graham Healy <grahamh220@gmail.com>
2016-02-02 15:27:09 +0000

    Converted XML configuration to Java

---
+++ src/main/java/com/graham/config/MvcConfig.java
@@ -0,0 +1,49 @@
+package com.graham.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"com.graham.*"})
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Bean
+	public InternalResourceViewResolver jspViewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
+	
+	public @Bean Mongo mongo() throws Exception {
+	      return new MongoClient("localhost");
+	  }
+
+	  public @Bean MongoTemplate mongoTemplate() throws Exception {
+	      return new MongoTemplate(mongo(), "mydatabase");
+	  }
+	
+	
+	
+}
