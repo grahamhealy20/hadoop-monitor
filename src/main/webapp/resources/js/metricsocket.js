33cbefd7bc9e66bc16a8bc8a72a5b48362107ecc
Graham Healy <grahamh220@gmail.com>
2016-04-19 19:00:06 +0100

    Websocket contention issues fixed

--- src/main/webapp/resources/js/metricsocket.js
+++ src/main/webapp/resources/js/metricsocket.js
@@ -1,51 +1,44 @@
 //WebSocket code
 var stompClient = null;
-var connected = false;
+var wait = false;
 
 //Setup Websocket
 
 function connect(clientId, metricsSuccessCallback, errorCallback) {
-		console.log("Connecting");
-		var socket = new SockJS( BASE_URL + '/realtime');
-		stompClient = Stomp.over(socket);
-
-		//Disable debug output
-		stompClient.debug = null;
-
-		stompClient.connect('', '', function(frame) {
-
-			//Subscribe to realtime metrics
-			stompClient.subscribe("/data/" + clientId, function(message){
-				var msg = JSON.parse(message.body);
-				console.log("sub 1");
-				metricsSuccessCallback(msg);
-			}, errorCallback);
-		}
+	var socket = new SockJS( BASE_URL + '/realtime');
+	stompClient = Stomp.over(socket);
+	//Disable debug output
+	stompClient.debug = null;
+	stompClient.connect('', '', function(frame) {
+		//Subscribe to realtime metrics
+		stompClient.subscribe("/data/" + clientId, function(message){
+			var msg = JSON.parse(message.body);
+			metricsSuccessCallback(msg);
+		}, errorCallback);
+	}
 	);
 }
 
-// Websocket for alerts
+//Websocket for alerts
 function connectAlerts(callback) {
-	console.log("Connecting");
-	var socket = new SockJS( BASE_URL + '/realtime');
-	stompClient = Stomp.over(socket);
 
-	//Disable debug output
-	stompClient.debug = null;
-
-	stompClient.connect('', '', function(frame) {
-
-		//Subscribe to alerts
-		stompClient.subscribe("/alerts/", function(data) {
-			console.log("Calling Alert Callack");
-			var alert = JSON.parse(data.body);
-			callback(alert);
-		}, function(error) {
-			console.log(error);
-		});
-	}
-);
-	
+	//Small delay, to prevent any resource allocation problems
+	setTimeout(function() {
+		var socket = new SockJS( BASE_URL + '/realtime');
+		stompClient = Stomp.over(socket);
+		//Disable debug output
+		stompClient.debug = null;
+		stompClient.connect('', '', function(frame) {
+			//Subscribe to alerts
+			stompClient.subscribe("/alerts/", function(data) {
+				var alert = JSON.parse(data.body);
+				callback(alert);
+			}, function(error) {
+				console.log(error);
+			});
+		}
+		);
+	}, 1000);
 }
 
 function disconnect() {
